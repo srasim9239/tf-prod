afafdf37cbf9171767bfc0bc8aa6ac405ca8109e
Rasim Shaykhullin <srasim9239@gmail.com>
2022-07-21 12:13:10 +0300

    var

--- variables.tf
+++ variables.tf
@@ -1,16 +1,16 @@
 variable "yandex_token" {
   description = "YC OAuth token"
-  default     = ""
+  default     = "AQAAAABHHr5xAATuwZ6laIKQfUlPsy3aJWeIZbU"
 
 }
 
 variable "yandex_cloud_id" {
   description = "ID of a cloud"
 
-  default     = ""
+  default     = "b1g12hg2u9h6gc47iq2j"
 }
 
 variable "yandex_folder_id" {
   description = "ID of a folder"
-  default     = ""
+  default     = "b1g9vmv0609dv2691p08"
 }
