5afb20ab0299182bdd24ca4146b5237746059415
Rasim Shaykhullin <srasim9239@gmail.com>
2022-07-22 13:14:27 +0300

    terrafor-ansible-test

--- main.tf
+++ main.tf
@@ -1,24 +1,24 @@
 terraform {
   required_providers {
     yandex = {
-      source = "yandex-cloud/yandex"
+      source  = "yandex-cloud/yandex"
       version = "0.76.0"
     }
   }
 }
 
-provider "yandex" { 
-   #service_account_id = "${var.yandex_token}"
-   token     = "${var.yandex_token}"
-   cloud_id  = "${var.yandex_cloud_id}"
-   folder_id = "${var.yandex_folder_id}"
-   zone      = "ru-central1-a"
+provider "yandex" {
+  # service_account_id = var.YC_SERVICE_ACCOUNT_KEY_FILE
+  token     = var.yandex_token
+  cloud_id  = var.yandex_cloud_id
+  folder_id = var.yandex_folder_id
+  zone      = "ru-central1-a"
 }
 
 
 resource "yandex_compute_instance" "vm-1" {
   name = "terraform1"
-  
+
 
   resources {
     cores  = 2
@@ -35,13 +35,15 @@ resource "yandex_compute_instance" "vm-1" {
     subnet_id = yandex_vpc_subnet.subnet-1.id
     nat       = true
   }
- 
 
+  metadata = {
+    ssh-keys = var.ssh_key
+  }
 }
 
 resource "yandex_compute_instance" "vm-2" {
   name = "terraform2"
-  
+
 
   resources {
     cores  = 4
@@ -59,7 +61,23 @@ resource "yandex_compute_instance" "vm-2" {
     nat       = true
   }
 
+  metadata = {
+    ssh-keys = var.ssh_key
+  }
+provisioner "remote-exec" {
+    inline = ["sudo apt update", "sudo apt install python3 -y", "echo Done!"]
 
+    connection {
+      host        = yandex_compute_instance.vm-2.network_interface.0.nat_ip_address
+      type        = "ssh"
+      user        = "root"
+      private_key = file(var.ssh_priv_key)
+    }
+  }
+
+  provisioner "local-exec" {
+    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u root -i '${yandex_compute_instance.vm-2.network_interface.0.nat_ip_address},' --private-key ${var.ssh_priv_key} -e 'pub_key=${var.ssh_key}' provision.yml"
+  }
 }
 
 resource "yandex_vpc_network" "network-1" {
